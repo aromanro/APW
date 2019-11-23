30a4e6356d9d4e376844c8c143b59f637a4c4916
Adrian Roman <aroman_ro@yahoo.com>
2019-11-23 14:07:48 +0200

    Moved pseudopotential functions in the Pseudopotential class

--- APW/BandStructure.cpp
+++ APW/BandStructure.cpp
@@ -95,41 +95,6 @@ namespace APW
 			kpoints[i] *= recVectPre;
 	}
 
-
-	double VeffCu(double R)
-	{
-		const double R2 = R * R;
-		const double R3 = R2 * R;
-		const double R4 = R2 * R2;
-
-		return 29. * exp(-2.3151241717834 * pow(R, 0.81266614122432) + 2.1984250222603E-2 * pow(R, 4.2246376280056))
-					- 0.15595606773483 * R - 3.1350051440417E-3 * R2 + 5.1895222293006e-2 * R3 - 2.8027608685637E-2 * R4;
-	}
-
-	// not used but might turn useful with the proper params
-	// use it as -StarkloffJoannopoulos(...) / r as the one for Cu
-	double StarkloffJoannopoulos(int Z, double r, double lambda, double rc)
-	{
-		return Z * (1. - exp(-lambda * r)) / (1. + exp(-lambda * (r - rc)));
-	}
-	
-
-	// the dumbest possible
-	// constant for R < Rl
-	// -Z/r otherwise (Z given by the number of valence electrons)
-	// don't add - or /r for this one
-	double VDumb(int Z, double R, double Rl, double C)
-	{
-		if (R < Rl) return C;
-
-		return -Z / R;
-	}
-
-
-	double VAl(double R)
-	{
-		return VDumb(3, R, 2.675, 1.3905 * 0.5);
-	}
 	
 	std::vector<std::vector<double>> BandStructure::Compute(const std::atomic_bool& terminate, const Options& options)
 	{	
@@ -171,7 +136,7 @@ namespace APW
 		{
 			//const double r = i * dr; // for uniform grid
 			const double r = Rp * (exp(i * deltaGrid) - 1.);
-			potential.m_potentialValues[i] = -VeffCu(r) / r;
+			potential.m_potentialValues[i] = -Pseudopotential::VeffCu(r) / r;
 			//potential.m_potentialValues[i] = pseudopotential.Value(r);
 		}
 
